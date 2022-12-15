755e0e577a8f942048cf14536930da47ac5cccaf
vsazonov <sazonovvv822@gmail.com>
2022-12-15 21:27:43 +0300

    2.2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -2,6 +2,7 @@ package app.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(basePackages = "app")
